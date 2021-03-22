4cf11ea8383ee00a1e9e81530387984850386b65
antoniusdr <antoniusr3101@gmail.com>
2021-03-22 16:25:16 +0100

    Added the standard pokemon that were mentioned and stylized them using bootstrap and sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <Title content="Simple title" />
-      </main>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to codaisseur</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>
-      </header>
-    </div>
+    <main className="container my-5">
+      <Title content="Spacing pog?" />
+      <div className="row">
+        {all_pokemon.map((pokemon, index) => (
+          <div key={index} className="col-md-6 col-lg-4">
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
 
