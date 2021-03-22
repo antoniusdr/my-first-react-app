39db5919ab2be91901cad10afdb7a65a272269ac
antoniusdr <antoniusr3101@gmail.com>
2021-03-22 13:44:32 +0100

    added a simple component and added to main App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title />
+      </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to codaisseur
-        </p>
+        <p>Welcome to codaisseur</p>
         <a
           className="App-link"
           href="https://reactjs.org"
